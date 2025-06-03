d2596ab78d0349403b880427aefd98d45ce2a9fb
Shunsuke Nakamachi <shunsuke.040222@gmail.com>
2025-06-03 09:51:16 +0000

    add publich service

--- orca_web_controller/orca_web_controller.py
+++ orca_web_controller/orca_web_controller.py
@@ -1,20 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Bool
 from std_srvs.srv import Empty
 
 class OrcaWebController(Node):
     def __init__(self):
         super().__init__('orca_web_controller')
 
-        # Publisher（Webから受け取って処理したいならsubscriberも必要）
-        self.pub = self.create_publisher(Int32, '/orca_00/control_state', 10)
+        # Publisher
+        self.state_pub = self.create_publisher(Int32, '/orca_00/control_state', 10)
+        self.enabled_state_pub = self.create_publisher(Bool, '/orca_00/enabled_state', 10)
 
         # Services
         self.set_offset_srv = self.create_service(Empty, '/orca_00/set_offset', self.handle_set_offset)
         self.enable_srv = self.create_service(Empty, '/orca_00/enable', self.handle_enable)
-        self.disable_srv = self.create_service(Empty, '/orca_00/disenable', self.handle_enable)
-        
+        self.disable_srv = self.create_service(Empty, '/orca_00/disenable', self.handle_disable)
+
         self.enabled = False
 
     def handle_set_offset(self, request, response):
@@ -22,7 +23,15 @@ class OrcaWebController(Node):
         return response
 
     def handle_enable(self, request, response):
+        self.enabled = True
         self.get_logger().info('orca_00  : enabled')
+        self.enabled_state_pub.publish(Bool(data=True))
+        return response
+
+    def handle_disable(self, request, response):
+        self.enabled = False
+        self.get_logger().info('orca_00  : disabled')
+        self.enabled_state_pub.publish(Bool(data=False))
         return response
 
 def main(args=None):
